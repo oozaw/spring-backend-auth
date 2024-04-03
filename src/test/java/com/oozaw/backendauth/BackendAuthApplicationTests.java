c9ede0211537baa857acb369a29e475e34192302
oozaw <79121990+oozaw@users.noreply.github.com>
2024-04-03 17:40:27 +0700

    init commit

---
+++ src/test/java/com/oozaw/backendauth/BackendAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oozaw.backendauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
